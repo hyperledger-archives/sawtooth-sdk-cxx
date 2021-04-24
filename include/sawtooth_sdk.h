6a116c8f7b1d28b8e86c55b52a65fe949cd35d62
Sean Deneen <77283555+seandeneen-gluwa@users.noreply.github.com>
2021-04-23 21:16:21 -0400

    Added start param to GetStatesByPrefix for paging

--- include/sawtooth_sdk.h
+++ include/sawtooth_sdk.h
@@ -164,7 +164,7 @@ class GlobalState {
 
     virtual ::google::protobuf::uint64 GetTip() const = 0;
 
-    virtual void GetStatesByPrefix(const std::string& address, std::string* root, std::vector<KeyValue>* out_values) const = 0;
+    virtual void GetStatesByPrefix(const std::string& address, std::string* root, std::string* start, std::vector<KeyValue>* out_values) const = 0;
     virtual void GetSigByNum(::google::protobuf::uint64 num, std::string* sig_out) const = 0;
     virtual void GetBlockById(const std::string& block_id, BlockInfo* header_out) const = 0;
     virtual void GetRewardBlockSignatures(const std::string& block_id, std::vector<std::string> &signatures, ::google::protobuf::uint64 first_pred, ::google::protobuf::uint64 last_pred) const = 0;
